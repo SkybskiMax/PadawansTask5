e51e91736e1dae99d6453b0ff22a4dbd233a53e1
Max <dranik15@mail.ru>
2019-02-17 23:13:53 +0300

    Update ArrayHelper.cs

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -5,14 +5,14 @@ namespace PadawansTask5
     public static class ArrayHelper
     {
         public static string CheckIfSymmetric(int[] source)
-        { 
-            if (source.Length==0)
-            {
-                throw new ArgumentException();
-            }
+        {
             if (source == null)
             {
                 throw new ArgumentNullException();
+            }
+            if (source.Length == 0)
+            {
+                throw new ArgumentException();
             }
             for (int i = 0; i<source.Length / 2; i++)
             {
